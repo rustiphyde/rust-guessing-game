33a4b6b0b332b896c19d47abfd6ae4a044f2de66
rustiphyde <rustiphyde@gmail.com>
2019-11-08 21:57:47 -0600

    Add compare function for guessed number to secret number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 /*brings in the input/output library into scope 
 from the standard library*/
 use std::io;
+/* brings in the Ordering comparison type from the 
+standard library*/
+use std::cmp::Ordering;
 /*brings in the random number generator trait 
 from the rand crate*/
 use rand::Rng;
@@ -42,15 +45,39 @@ fn main(){
      */
     io::stdin().read_line(&mut guess)
     /*expect handles the Err variant of  the 
-    io::Result of read_line*/
+    io::Result enum of read_line*/
         .expect("Failed to read line");
+
+    /*shadows the previous value of guess with a new one
+    bound to the expression which takes the original
+    guess value minus whitespace from pressing enter
+     and parsed into an unsigned 32 bit integer type 
+     annotated after the :*/
+     /*If a number is not provided the expect method
+     of the parse Result fires off*/
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
     /*{} is a placeholder that represents the 
     values in order after the format string, so here
     {} will be replaced with the value of 'guess'
-    that comes from the user input 
-     */
+    that comes from the user input */
     println!("You guessed: {}", guess);
+
+    /*match expression is made up of arms. An arm 
+    consists of a pattern and the code that should be 
+    run if the value given to the beginning of the 
+    match expression fits that arm’s pattern.*/
+    /*cmp compares two values - the start value (guess)
+    and the reference value argument(&secret_number)*/
+    /*The Ordering enum has the 3 variants that are
+    possible when comparing two values - Less, Greater
+    and Equal*/
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small! The number was {}", secret_number),
+        Ordering::Greater => println!("Too big!The number was {}", secret_number),
+        Ordering::Equal => println!("You guessed the secret number! Congratulations, you win!"),
+    }
 }
 
 
